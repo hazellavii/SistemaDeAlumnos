cdafb3709002ac3d8131cfa81f69b40be70f82b3
hazellavii <hazellavi.1@gmail.com>
2026-09-21 15:12:27 -0300

    Etapa 2 agregar constructor a Alumno

--- Alumno.cs
+++ Alumno.cs
@@ -5,4 +5,13 @@ public class Alumno
     public int Legajo { get; set; }
     public double Nota1 { get; set; }
     public double Nota2 { get; set; }
+    
+    // ETAPA 2 //
+    public Alumno(string nombre, int legajo, double nota1, double nota2)
+    {
+        Nombre = nombre;
+        Legajo = legajo;
+        Nota1 = nota1;
+        Nota2 = nota2;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,25 +1,7 @@
-﻿// ETAPA 1 //
+﻿// ETAPA 2 //
 
-Alumno alumno1 = new Alumno();
-
-alumno1.Nombre = "Abi";
-alumno1.Legajo = 1234;
-alumno1.Nota1 = 8.5;
-alumno1.Nota2 = 7;
-
-Alumno alumno2 = new Alumno();
-
-alumno2.Nombre = "Juli";
-alumno2.Legajo = 5678;
-alumno2.Nota1 = 6;
-alumno2.Nota2 = 9.5;
-
+Alumno alumno1 = new Alumno("Abi", 1234, 8.5, 7);
+Alumno alumno2 = new Alumno("Juli", 5678, 6, 9.5);
 
 Console.WriteLine(alumno1.Nombre + " - " + alumno1.Legajo);
 Console.WriteLine(alumno2.Nombre + " - " + alumno2.Legajo);
-
-alumno1.Nombre = "Abi";
-
-Console.WriteLine("Después del cambio:");
-Console.WriteLine(alumno1.Nombre);
-Console.WriteLine(alumno2.Nombre);
