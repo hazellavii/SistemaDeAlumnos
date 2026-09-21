b721edb1ec5962ea5d1f51b584312ef884cb7ae0
hazellavii <hazellavi.1@gmail.com>
2026-09-21 10:17:25 -0300

    Etapa 1: crear clase Alumno

---
+++ Alumno.cs
@@ -0,0 +1,8 @@
+
+public class Alumno
+{
+    public string Nombre { get; set; }
+    public int Legajo { get; set; }
+    public double Nota1 { get; set; }
+    public double Nota2 { get; set; }
+}
