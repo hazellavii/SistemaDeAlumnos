b872ce2be5d83d5b451ee7a694b8b8ac46274938
hazellavii <hazellavi.1@gmail.com>
2026-09-21 15:18:33 -0300

    Etapa 3 agregar métodos de Alumno

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿// ETAPA 2 //
+﻿// ETAPA 3 //
 
-Alumno alumno1 = new Alumno("Abi", 1234, 8.5, 7);
-Alumno alumno2 = new Alumno("Juli", 5678, 6, 9.5);
+/////A1
+Alumno alumno1 = new Alumno("Brookie", 1234, 8, 6);
+Alumno alumno2 = new Alumno("Luchi", 5678, 4, 5);
 
-Console.WriteLine(alumno1.Nombre + " - " + alumno1.Legajo);
-Console.WriteLine(alumno2.Nombre + " - " + alumno2.Legajo);
+Console.WriteLine(alumno1.Nombre + " - Promedio: " + alumno1.Promedio());
+Console.WriteLine("¿Está aprobada? " + alumno1.EstaAprobado());
+
+Console.WriteLine();
+
+Console.WriteLine(alumno2.Nombre + " - Promedio: " + alumno2.Promedio());
+Console.WriteLine("¿Está aprobada? " + alumno2.EstaAprobado());
+
+/////A2
+alumno2.SubirNota();
+
+Console.WriteLine();
+Console.WriteLine("Notas de Luchi después de subirlas:");
+Console.WriteLine("Nota 1: " + alumno2.Nota1);
+Console.WriteLine("Nota 2: " + alumno2.Nota2);
+Console.WriteLine("Nuevo promedio: " + alumno2.Promedio());
+Console.WriteLine("¿Está aprobada? " + alumno2.EstaAprobado());
